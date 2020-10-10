f5e14accb1ddf2f831c5d337edf50487f7ff8cc2
Marcio Smith <marciosmith@guest1s-MacBook-Pro.local>
2020-10-10 14:27:59 -0400

    adding anonymous reply functionality

---
+++ helpers/anonymous.js
@@ -0,0 +1,30 @@
+export default function Anonymous (client, spreadsheet) {
+  client.on('message', msg => {
+    const { author, content } = msg;
+    const anonCommand = content.includes('!anon');
+    if (msg.channel.type !== 'dm' || !anonCommand) {
+      return;
+    }
+  
+    const { username, id } = author;
+    
+    (async () => {
+      await spreadsheet.loadInfo();
+      const sheet = spreadsheet.sheetsByIndex[0];
+      await sheet.addRows([
+        { 
+          id,
+          user: username,
+          message: content
+        }
+      ])
+
+      const channel = client.channels.cache.get(`764292841236594728`)
+      channel.send(`Heyyo Wolfy Here!!! I have a message that was sent anonymously: \n **${content.replace('!anon ', '')}**.`);
+    
+      msg.reply(`Your Message Has Been Sent Anonymously To The ${channel.name} Channel` );
+    })();
+  });
+  
+  console.log('Anonymous Functionality Added 1/1');
+}
--- wolfy.js
+++ wolfy.js
@@ -1,1 +1,34 @@
-console.log('testing')
+import Discord from 'discord.js';
+import dotenv from 'dotenv'
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import Anonymous from './helpers/anonymous';
+
+dotenv.config({path: './.env'})
+const {
+  DISCORD_TOKEN,
+  GOOGLE_SERVICE_ACCOUNT_EMAIL,
+  GOOGLE_PRIVATE_KEY,
+  ANONYMOUS_SHEET_ID,
+} = process.env
+
+const anonSpreadsheet = new GoogleSpreadsheet(ANONYMOUS_SHEET_ID);
+
+
+(async () => {
+  // use service account creds
+  await anonSpreadsheet.useServiceAccountAuth({
+    client_email: GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    private_key: GOOGLE_PRIVATE_KEY.replace(/\\n/gm, '\n'),
+  }, GOOGLE_SERVICE_ACCOUNT_EMAIL);
+
+  Anonymous(client, anonSpreadsheet)
+})();
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+client.login(DISCORD_TOKEN);
